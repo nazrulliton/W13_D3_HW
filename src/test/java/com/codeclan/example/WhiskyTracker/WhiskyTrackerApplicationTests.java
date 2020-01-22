59d937f03e2f6000a2981c97ae655b4a83626463
naz_liton <nazrulliton@icloud.com>
2020-01-22 17:51:58 +0000

    two mvp qs working

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,40 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
-
+	@Autowired
+	WhiskyRepository whiskyRepository;
+	@Autowired
+	DistilleryRepository distilleryRepository;
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void findWhiskyByYear(){
+		List<Whisky> found = whiskyRepository.findWhiskyByYear(1991);
+		assertEquals("The Rosebank 12 - Flora and Fona", found.get(0).getName());
+	}
+
+	@Test
+	public void findDistilleryByRegion(){
+		List<Distillery> found = distilleryRepository.findDistilleryByRegion("Lowland");
+		assertEquals("Rosebank", found.get(0).getName());
 	}
 
 }
